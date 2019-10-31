62aec5638f98152cae431984667a0cfea28737f4
Arpan Laha <arpanlaha99@gmail.com>
2019-10-31 22:36:24 +0000

    fix random shit

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -11,7 +11,7 @@ import "../public/style.scss";
 /**
  * The home page.
  */
-export default class App extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
