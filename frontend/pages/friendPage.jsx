80171b244ad098239eaaf369bd1ba4b68403e77e
Alice Fang <alicesf2@illinois.edu>
2019-10-30 23:19:43 -0500

    initial friends page w/no styling

---
+++ frontend/pages/friendPage.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { 
+    Button, 
+    Row, 
+    Col, 
+    Container, 
+    Card, 
+    CardBody, 
+    CardTitle, 
+    CardText, 
+    Modal, 
+    ModalBody,
+    ModalFooter,
+    Form, 
+    Input, 
+    FormGroup, 
+    Label } from 'reactstrap'
+
+class FriendPage extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        friends: ['Hyunsoo', 'Eric', 'Arpan', 'Alice'],
+        modalOpen: false
+      }
+    }
+
+    openModal = () => {
+        this.setState({
+            modalOpen: true
+        })
+    }
+
+    closeModal = () => {
+        this.setState({
+            modalOpen: false
+        })
+    }
+
+    addFriend = () => {
+        let friendsArr = this.state.friends
+        friendsArr.push(this.state.newFriend)
+        this.setState({
+            friends: friendsArr
+        })
+    }
+
+    handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        })
+      }
+
+    handleKeyPress = () => {
+
+    }
+    render() {
+        return (
+            <Container>
+                <h1 align='center'>Your Friends</h1>
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label>Name</Label>
+                                <Input
+                                    name="newFriend"
+                                    placeholder="Enter Name"
+                                    onChange={this.handleChange}
+                                    onKeyPress={this.handleKeyPress}
+                                />
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                        <ModalFooter>
+                        <Button color="secondary" onClick={this.closeModal}>
+                            Cancel
+                        </Button>
+                        <Button color="primary" onClick={this.addFriend}>
+                            Submit
+                        </Button>
+                        </ModalFooter>
+                </Modal>
+                <Button color='primary' onClick={this.openModal}>Add New Friend</Button>
+                <Button color='secondary'>Logout</Button>
+                
+                <Row>
+                { this.state.friends.map(friend => (
+                    <Col md='4'>
+                        <Card>
+                            <CardBody>
+                                <CardTitle>
+                                    {friend}
+                                </CardTitle>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                ))}
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default FriendPage
