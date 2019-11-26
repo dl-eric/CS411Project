33e8d181a2a855409dbe7c26ca9df578470c23c1
Arpan Laha <arpanlaha99@gmail.com>
2019-11-26 21:05:10 +0000

    fix css link

--- frontend/components/Head.jsx
+++ frontend/components/Head.jsx
@@ -28,7 +28,9 @@ export default class Head extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
         />
         <title>
           {`${
