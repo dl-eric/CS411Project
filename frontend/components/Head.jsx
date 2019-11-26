254e2fb3db93ad8dba3f1bd2b2915fa31111c7cc
Arpan Laha <arpanlaha99@gmail.com>
2019-11-26 21:34:58 +0000

    fix lint errors

--- frontend/components/Head.jsx
+++ frontend/components/Head.jsx
@@ -30,12 +30,12 @@ export default class Head extends Component {
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <title>
           {`${
             this.props.title ? `${this.props.title} | ` : ""
-          }CS 411 Final Project`}
+            }CS 411 Final Project`}
         </title>
       </NextHead>
     );
