e39c3ba20328b7edd9e2b0f6a751b8db4b225afc
Arpan Laha <arpanlaha99@gmail.com>
2019-12-02 16:34:11 +0000

    forgot to add renamed file

---
+++ backend/api_mongo/views/main_mongo.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, request, jsonify
+from api_mongo.models import db, Person, Email
+from api_mongo.core import create_response, serialize_list, logger
+
+main_mongo = Blueprint("main_mongo", __name__)  # initialize blueprint
+
+# function that is called when you visit /
+@main_mongo.route("/")
+def index():
+    # you are now in the current application context with the main.route decorator
+    # access the logger with the logger from api.core and uses the standard logging module
+    # try using ipdb here :) you can inject yourself
+    logger.info("Hello World!")
+    return "Hello World!"
+
+
+# function that is called when you visit /persons
+@main_mongo.route("/persons", methods=["GET"])
+def get_persons():
+    persons = Person.objects()
+    return create_response(data={"persons": persons})
+
+
+# POST request for /persons
+@main_mongo.route("/persons", methods=["POST"])
+def create_person():
+    data = request.get_json()
+
+    logger.info("Data recieved: %s", data)
+    if "name" not in data:
+        msg = "No name provided for person."
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    if "emails" not in data:
+        msg = "No email provided for person."
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+
+    #  create MongoEngine objects
+    new_person = Person(name=data["name"])
+    for email in data["emails"]:
+        email_obj = Email(email=email)
+        new_person.emails.append(email_obj)
+    new_person.save()
+
+    return create_response(
+        message=f"Successfully created person {new_person.name} with id: {new_person.id}"
+    )
